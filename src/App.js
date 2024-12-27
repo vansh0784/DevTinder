35dfc39ed4c133370df30b19145c5c4d39d3162a
vansh <vanshsin784@gmail.com>
2024-12-27 19:47:07 +0530

    Exploring routes with HTTP protocols

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 const express=require("express");
 const app=express();
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Vansh",
+        secondName:"Singh"
+    });
+});
+app.post("/user",(req,res)=>{
+    res.send("User Added to Database Successfully!");
+});
+app.delete("/user",(req,res)=>{
+    res.send("User deleted succesfully!");
+});
+app.patch("/user",(req,res)=>{
+    res.send("Adding a minor Change");
+});
+app.put("/user",(req,res)=>{
+    res.send("Changes have been made in DB");
+});
 app.use("/test",(req,res)=>{
     res.send("Hello I m Testing the server");
-})
-app.use("/",(req,res)=>{
-    res.send("Welcome To Server!");
-})
-app.use("/helllo",(req,res)=>{
-    res.send("Hello Hello Hello !");
-})
+});
+
 app.listen(3000 ,()=>{console.log("server is running properly")});
+// Here order is really matter in code .. because server run it line by line
