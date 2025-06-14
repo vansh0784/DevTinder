2905476d2ffe862a6a4836fc69591ccf31744a11
vansh <vanshsin784@gmail.com>
2025-06-14 05:35:32 +0530

    updating socket url

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,7 +3,7 @@ const socket = require("socket.io");
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://devtinder-ui-theta.vercel.app",
     },
   });
 
